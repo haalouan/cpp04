30a0abc16ed4a24a7c9814ea68d7d3a2e4fddf86
haalouan <haalouan@student.1337.ma>
2025-02-13 16:56:27 +0100

    ex00 good

---
+++ ex00/Cat.hpp
@@ -0,0 +1,17 @@
+#ifndef Cat_HPP
+#define Cat_HPP
+
+#include "Animal.hpp"
+
+class Cat : public Animal {
+
+    public:
+        Cat();
+        Cat(const Cat& other);
+        Cat& operator=(const Cat& other);
+        ~Cat();
+
+        void makeSound() const ;
+
+};
+#endif
